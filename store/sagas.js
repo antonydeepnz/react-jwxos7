2f8c9356ef448af9db4290da9627872db1a8b92e
antonydeepnz <mashoshini@gmail.com>
2020-03-10 12:06:43 +0300

    1 file modified

--- store/sagas.js
+++ store/sagas.js
@@ -1,9 +1,16 @@
 import { put, call, takeEvery } from 'redux-saga'
+import { getGenres, requestGenres } from './actions'
 
 function* getGenres(){
   try{
-    
+    const res = yield call()
   } catch (e){
     yield console.log(e)
   }
 }
+
+function* request(){
+  yield takeEvery(requestGenres, getGenres)
+}
+
+export default request
