bf37d530e02048071f9baeac00180ac55fd8a1bc
antonydeepnz <mashoshini@gmail.com>
2020-03-11 09:13:55 +0300

    2 files modified

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { render } from 'react-dom';
 import { Provider, useSelector, useDispatch } from 'react-redux'
 import './style.css';
 
 import { requestGenres } from './store/actions'
+import { requestFilms } from './store/actionRequestActions'
+
 
 import store from './store/store'
 //https://material.io/resources/icons/?search=chevron&icon=chevron_right&style=baseline
 
 const App = () => {
   const genres = useSelector(state => state.genres.data)
-  //const genre = useSelector(state => state.genres.data.id === props.id)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(requestGenres())
+    dispatch(requestFilms())
+  }, [])
+
   return(
     <>
       {
         genres.map(i => (<li key={i.id}>{i.name}</li>))
       }
-      <button onClick={() => dispatch(requestGenres())}>Click</button>
+      
     </>
   )
 }
+//<button onClick={() => dispatch(requestGenres())}>Click</button>
 
 render(
 <Provider store={store}>
--- store/filmRequestSagas.js
+++ store/filmRequestSagas.js
@@ -3,9 +3,21 @@ import axios from 'axios'
 
 import { requestFilms, getFilms, getErrors } from './actionRequestActions'
 
+export const CORS_AVOID = 'https://cors-anywhere.herokuapp.com/' 
+export const BASIC_URL = `https://api.themoviedb.org/3/`
+export const API_KEY = 'api_key=47e345218071181a1ca1d4eb072cc0cf'
+export const LANGUAGE = (lang) => `&language=${lang}`
+export const GET_IMAGE = (link, width) => `https://image.tmdb.org/t/p/w${width}/${link}`
+
+const Axios = axios.create({
+  baseURL: `${BASIC_URL}`,
+  timeout: 1000
+})
+
 function* get_Films(){
+  const query = 'Matrix'
   try {
-    const result = yield call()
+    const result = yield call(Axios.get, `search/movie?${API_KEY}&language=ru-RU&query=${query}`)
     put(getFilms(result.data))
   } catch (err){
     yield put(getErrors(err))
